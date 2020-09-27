911665f1cfc2925eb35760b4cdac97c6c6910115
ferdousara-parvin <parvin.ferdousara@gmail.com>
2020-09-27 15:58:56 -0400

    More cleaning up

--- src/HTTPClient.java
+++ src/HTTPClient.java
@@ -11,32 +11,38 @@ public class HTTPClient {
     private BufferedReader in;
 
     public static void main(String[] args) {
-//        // Verify validity of command
-//        if (args.length < 2) {
-//            System.out.println("Missing some parameters");
-//            System.exit(0);
-//        }
-//
-//        if(args[1].equalsIgnoreCase("help")) { // Help messages
-//            if(args.length == 2) {
-////                HttpcHelp.printHelpMessage();
-//            }
-//            else {
-////                HttpcHelp.printMethodHelpMessage(args[2]);
-//            }
-//
-//            System.exit(0);
-//        }
+//        parseRequest(args);
 
-
+        // Testing purposes
         Request request =  new Request("www.google.com",
                 "/search?sxsrf=ALeKk02K4A2OBcJYap3QKIQrqpU5MClZaw%3A1601219232074&source=hp&ei=oKpwX83_AYW3gger4rXIDA&q=cats&oq=cats&gs_lcp=CgZwc3ktYWIQAzIKCC4QsQMQQxCTAjICCAAyBQgAELEDMgUILhCxAzICCC4yBQguELEDMgUILhCxAzIICC4QsQMQgwEyCAgAELEDEIMBMggILhCxAxCDAToECCMQJzoFCAAQkQI6CwguELEDEMcBEKMCOg4ILhCxAxCDARDHARCjAjoHCC4QsQMQQzoECAAQQzoICC4QxwEQowJQjwNYxwVgigdoAHAAeAGAAcIBiAGFBJIBAzAuNJgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwjNz6G8zonsAhWFm-AKHStxDckQ4dUDCAg&uact=5",
-                HTTPMethod.GET, new HashMap<String, String>());
+                HTTPMethod.GET,
+                new HashMap<String, String>());
         new HTTPClient(request);
 
     }
 
-    //httpheader -> dict(string:string)
+    public static void parseRequest(String[] args) {
+        // Verify validity of command
+        if (args.length < 2) {
+            System.out.println("Missing some parameters");
+            System.exit(0);
+        }
+
+        if(args[1].equalsIgnoreCase("help")) { // Help messages
+            if(args.length == 2) {
+//                HttpcHelp.printHelpMessage();
+            }
+            else {
+//                HttpcHelp.printMethodHelpMessage(args[2]);
+            }
+
+            System.exit(0);
+        }
+
+        // more to add here
+    }
+
     public HTTPClient(Request request){
         try {
             // Connect to the server
