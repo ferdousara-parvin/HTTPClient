660d712cfcb516572521886e2d65c50729161c6e
Viveka <viveka98@hotmail.ca>
2020-11-29 20:39:47 -0500

    Fixing the equence number proble in the sender side
    --
    Co-authored-by: ferdousara-parvin <32084900+ferdousara-parvin@users.noreply.github.com>.

--- src/Helpers/UDPConnection.java
+++ src/Helpers/UDPConnection.java
@@ -1,8 +1,5 @@
 package Helpers;
 
-import Client.HttpClientLibrary;
-import Client.Requests.Request;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.*;
@@ -11,10 +8,10 @@ import java.util.*;
 import java.util.logging.Logger;
 
 public class UDPConnection {
-    public final static int WINDOW_SIZE = 5;
-    public final static int MAX_SEQUENCE_NUMBER = 20;
+    public final static int WINDOW_SIZE = 3;
+    public final static int MAX_SEQUENCE_NUMBER =10000 * WINDOW_SIZE;
     public final static int MAX_PAYLOAD_SIZE = Packet.MAX_LEN - Packet.MIN_LEN;
-    public final static long DELAY_BEFORE_TIMEOUT = 1000;
+    public final static long DELAY_BEFORE_TIMEOUT = 100000;
     public final static SocketAddress routerAddress = new InetSocketAddress("localhost", 3000);
 
     private static final Logger logger = Logger.getLogger(UDPConnection.class.getName());
@@ -29,7 +26,7 @@ public class UDPConnection {
     private static int rcv_base = 0;
     private static int rcv_tail = UDPConnection.WINDOW_SIZE - 1;
     private static ArrayList<Packet> finalPacketsInOrder = new ArrayList<>();
-    private static ArrayList<Packet> packetsInBuffer = new ArrayList<>();
+    private static ArrayList<Packet> packetsInBuffer = new ArrayList<>(MAX_SEQUENCE_NUMBER);
 
     // -----------BUILD PACKETS---------------------
 
@@ -161,7 +158,7 @@ public class UDPConnection {
     }
 
     public static int getRandomSequenceNumber() {
-        return (int)(Math.random()*100);
+        return (int) (Math.random() * 100) + 500;
     }
 
     // --------------SELECTIVE REPEAT------------------------------
@@ -173,18 +170,18 @@ public class UDPConnection {
         Collections.fill(sentList, Boolean.FALSE);
 
         // Send data packets using selective repeat
-        while(ackList.contains(false)) {
+        while (ackList.contains(false)) {
             sendWindow(packets, socket);
 
             Packet response = receivePacket(socket);
             if (response != null && response.getType() == PacketType.ACK.value) {
-                ackList.set((int) response.getSequenceNumber()-1, true);
+                ackList.set((int) response.getSequenceNumber() - 1, true);
                 // Slide window
                 if (windowTail < ackList.size() && ackList.get(windowHead)) {
                     int newWindowHead = windowHead;
                     int newWindowTail = windowTail;
-                    for(int i = windowHead; i <= windowTail; i++) {
-                        if(ackList.get(i)) {
+                    for (int i = windowHead; i <= windowTail; i++) {
+                        if (ackList.get(i)) {
                             newWindowHead += 1;
                             newWindowTail += 1;
                         } else {
@@ -234,7 +231,8 @@ public class UDPConnection {
         }
 
         public void run() {
-            if(!ackList.get(indexInAckList)) {
+            // ackList can be null when the last packet has been already acknowledged and the vars for the SR have been reset
+            if (ackList != null && !ackList.get(indexInAckList)) {
                 logger.info("Packet #" + packetToBeSentAgain.getSequenceNumber() + " has been resent due to timeout");
                 UDPConnection.sendPacket(packetToBeSentAgain, socket);
 
@@ -247,6 +245,24 @@ public class UDPConnection {
 
     // --------------SELECTIVE REPEAT------------------------------
 
+    // seq in [head -n, head - 1]
+
+    private static boolean inRange(int number, int lowerBound, int upperBound) {
+        return number >= lowerBound && number <= upperBound;
+    }
+
+    private static boolean isSequenceNumberInPreviousWindow(int sequenceNumber) {
+        // if (h - n < 0)
+        if (rcv_base - WINDOW_SIZE < 0) {
+//            [0, h-1] || [(h-n)%MAX, MAX -1]
+            return inRange(sequenceNumber, 0, rcv_base - 1)
+                    || inRange(sequenceNumber, (rcv_base - WINDOW_SIZE) % MAX_SEQUENCE_NUMBER, MAX_SEQUENCE_NUMBER - 1);
+        }
+
+        return inRange(sequenceNumber, rcv_base - WINDOW_SIZE, rcv_base - 1);
+    }
+
+
     public static ArrayList<Packet> receiveAllPackets(DatagramSocket socket) {
         Packet receivedPacket = receivePacket(socket);
 
@@ -254,25 +270,30 @@ public class UDPConnection {
         while (receivedPacket != null && receivedPacket.getType() != PacketType.FIN.value) {
             if (receivedPacket.getType() == PacketType.DATA.value) {
                 // Packet with sequence number b/w rcv_base and rcv_base+N-1 where N = window size
-                if (receivedPacket.getSequenceNumber() >= rcv_base && receivedPacket.getSequenceNumber() <= rcv_tail) {
-                    UDPConnection.sendACK(receivedPacket.getSequenceNumber() + 1, receivedPacket.getPeerPort(), receivedPacket.getPeerAddress(), socket);
-                    packetsInBuffer.add(receivedPacket.getSequenceNumber(), receivedPacket);
-                    if (receivedPacket.getSequenceNumber() == rcv_base) {
-                        for (int i = rcv_base; i < packetsInBuffer.size() && packetsInBuffer.get(i) != null; i++, rcv_base++) {
-                            finalPacketsInOrder.add(packetsInBuffer.get(i));
-                            packetsInBuffer.set(i, null);
-                        } // TODO: modular shit for packetBuffer
+                if (rcv_tail > rcv_base) {
+                    // seq in [head, tail]
+                    if (receivedPacket.getSequenceNumber() >= rcv_base && receivedPacket.getSequenceNumber() <= rcv_tail) {
+                        addPacketInBuffer(socket, receivedPacket);
                     }
-
-                    rcv_tail = rcv_base + UDPConnection.WINDOW_SIZE - 1;
+                    // seq in [head -n, head - 1]
+                    else if (isSequenceNumberInPreviousWindow(receivedPacket.getSequenceNumber())) {
+                        UDPConnection.sendACK(receivedPacket.getSequenceNumber() + 1, receivedPacket.getPeerPort(), receivedPacket.getPeerAddress(), socket);
+                    }
+                } else if (rcv_tail < rcv_base) {
+                    // seq in [h, MAX-1] || [0, t]
+                    if (receivedPacket.getSequenceNumber() >= rcv_base && receivedPacket.getSequenceNumber() <= MAX_SEQUENCE_NUMBER - 1
+                            || receivedPacket.getSequenceNumber() >= 0 && receivedPacket.getSequenceNumber() <= rcv_tail) {
+                        // Send ACK
+                        addPacketInBuffer(socket, receivedPacket);
+                    }
+                    // seq in [head -n, head - 1]
+                    else if (isSequenceNumberInPreviousWindow(receivedPacket.getSequenceNumber())) {
+                        UDPConnection.sendACK(receivedPacket.getSequenceNumber() + 1, receivedPacket.getPeerPort(), receivedPacket.getPeerAddress(), socket);
+                    }
+                } else {
+                    //ERROR: CRASH
                 }
 
-                // Packet with sequence number b/w rcv_base-N and rcv_base-1 where N = window size
-                else if (receivedPacket.getSequenceNumber() >= rcv_base - (UDPConnection.WINDOW_SIZE - 1) && receivedPacket.getSequenceNumber() <= rcv_base - 1) {
-                    UDPConnection.sendACK(receivedPacket.getSequenceNumber() + 1, receivedPacket.getPeerPort(), receivedPacket.getPeerAddress(), socket);
-                }
-
-                // else ignore packet
             }
 
             receivedPacket = UDPConnection.receivePacket(socket);
@@ -285,6 +306,26 @@ public class UDPConnection {
         return finalPacketsInOrder;
     }
 
+    private static void addPacketInBuffer(DatagramSocket socket, Packet receivedPacket) {
+        // Send ACK
+        UDPConnection.sendACK(receivedPacket.getSequenceNumber() + 1, receivedPacket.getPeerPort(), receivedPacket.getPeerAddress(), socket);
+
+        // Buffer packet
+        packetsInBuffer.add(receivedPacket.getSequenceNumber(), receivedPacket);
+
+        //Slide Window
+        if (receivedPacket.getSequenceNumber() == rcv_base) {
+            for (int i = rcv_base; i < packetsInBuffer.size() && packetsInBuffer.get(i) != null; i++) {
+                finalPacketsInOrder.add(packetsInBuffer.get(i));
+                packetsInBuffer.set(i, null);
+                rcv_base = (rcv_base + 1) % MAX_SEQUENCE_NUMBER;
+            }
+
+        }
+
+        rcv_tail = (rcv_base + UDPConnection.WINDOW_SIZE - 1) % MAX_SEQUENCE_NUMBER;
+    }
+
     private static void resetVars() {
         windowHead = 0;
         windowTail = UDPConnection.WINDOW_SIZE - 1;
@@ -294,6 +335,6 @@ public class UDPConnection {
         rcv_base = 0;
         rcv_tail = UDPConnection.WINDOW_SIZE - 1;
         finalPacketsInOrder = new ArrayList<>();
-        packetsInBuffer = new ArrayList<>();
+        packetsInBuffer = new ArrayList<>(MAX_SEQUENCE_NUMBER);
     }
 }
